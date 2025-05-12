160fde7c0430cbcb650c04913c533328606a2715
Tommy Syllow <tommysyllow@gmail.com>
2025-05-12 16:09:10 +0200

    wip

--- app.py
+++ app.py
@@ -4,8 +4,22 @@ import pandas as pd
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import re
+import plotly.graph_objects as go
+import pandas as pd
 
 # --- Read in data ---
-data_folder = "../data/"
-transaction_data = pd.read_csv("reduced_transaction_data.csv", sep=",",  encoding="utf8")
+data_folder = "data/"
+transaction_data = pd.read_csv(data_folder + "reduced_transaction_data.csv", sep=",",  encoding="utf8")
 
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+fig = go.Figure(data=[go.Table(
+    header=dict(values=list(transaction_data.columns),
+                fill_color='paleturquoise',
+                align='left'))
+])
+
+fig.show()
+
+if __name__ == '__main__':
+    app.run(debug=True)
